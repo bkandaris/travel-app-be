81dd324e6ec9a8b335187c044ed7cb70266cb700
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-30 15:25:46 -0500

    please work

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt-nodejs');
 
 //reg
 router.post('/register', async (req, res) => {
   try {
-    //generate new password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    //create new user
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPassword,
+      password: req.body.password,
     });
 
     //save user and respond
@@ -31,12 +25,15 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
     !user && res.status(400).json('wrong credentials');
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+    // const validPassword = await bcrypt.compare(
+    //   req.body.password,
+    //   user.password
+    // );
+    if (req.body.password == user.password) {
+      res.status(200).json(user);
+    }
 
-    !validPassword && res.status(400).json('wrong credentials');
+    // !validPassword && res.status(400).json('wrong credentials');
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
