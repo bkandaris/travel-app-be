8fae5fb6d1b4b3c600a0d1e84de28fd19b262405
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-30 16:05:53 -0500

    please work

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
 // CLUSTER 1 - Mongodb
 const app = express();
 const pinRoute = require('./routes/pins');
@@ -8,7 +7,6 @@ const userRoute = require('./routes/users');
 
 const cors = require('cors');
 app.use(cors());
-dotenv.config();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
