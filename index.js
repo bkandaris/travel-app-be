6d1dd53261551a7885d6a08f9e0b4759db0a15b0
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-30 15:15:14 -0500

    added cors

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const app = express();
 const pinRoute = require('./routes/pins');
 const userRoute = require('./routes/users');
 
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URL)
